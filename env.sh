10ba0de952a38afdb7915935a6ba2be4636d708c
Quang Dat <quangdat2000.pham@gmail.com>
2020-11-18 09:04:46 +0700

    Remove env.sh docker-compose.yaml .gitpod.yml

--- env.sh
+++
@@ -1,6 +0,0 @@
-export HOST_WWW="8180"
-export HOST_OTHER="8181"
-export HOST_DOCKER="/var/run/docker.sock"
-export HOST_JENKINS_DATA="/srv/www/jenkins"
-export HOST_UID="1004"
-export HOST_GID="999"
